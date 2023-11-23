566667b3d646c4bb07138a291fcce7ca0ff0fabe
unknown <dianaditya200@gmail.com>
2023-11-23 17:00:15 +0700

    revisi warna font

--- client_side/src/views/AboutView.vue
+++ client_side/src/views/AboutView.vue
@@ -19,30 +19,30 @@ export default {
 
 <template>
   <div class="flex items-center justify-center align-center">
-    <select @change="pilihJenjang(this.index)" v-model="index">Pilih jenjang
+    <select style="color: black;" @change="pilihJenjang(this.index)" v-model="index">Pilih jenjang
       <option selected disabled>Pilih jenjang</option>
       <option>{{ jenjang[0] }}</option>
       <option>{{ jenjang[1] }}</option>
       <option>{{ jenjang[2] }}</option>
       <option>{{ jenjang[3] }}</option>
     </select>
-    <select v-if="this.index === jenjang[0]">Pilih jenjang
+    <select style="color: black;" v-if="this.index === jenjang[0]">Pilih jenjang
       <option>{{ praSekolah[0] }}</option>
       <option>{{ praSekolah[1] }}</option>
       <option>{{ praSekolah[2] }}</option>
     </select>
-    <select v-if="this.index === jenjang[1]">Pilih jenjang
+    <select style="color: black;" v-if="this.index === jenjang[1]">Pilih jenjang
       <option>{{ dasar[0] }}</option>
       <option>{{ dasar[1] }}</option>
       <option>{{ dasar[2] }}</option>
       <option>{{ dasar[3] }}</option>
     </select>
-    <select v-if="this.index === jenjang[2]">Pilih jenjang
+    <select style="color: black;" v-if="this.index === jenjang[2]">Pilih jenjang
       <option>{{ menengah[0] }}</option>
       <option>{{ menengah[1] }}</option>
       <option>{{ menengah[2] }}</option>
     </select>
-    <select v-if="this.index === jenjang[3]">Pilih jenjang
+    <select style="color: black;" v-if="this.index === jenjang[3]">Pilih jenjang
       <option>{{ tinggi[0] }}</option>
       <option>{{ tinggi[1] }}</option>
       <option>{{ tinggi[2] }}</option>
